a9392452c0d92cc59288b3739df661280b64ced6
Fabiano Cunha <cunhasb@aol.com>
2018-01-09 16:26:17 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
 export default (state = [], action) => {
-  return state;
-}
+  let index;
+  let quote;
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      // console.log("quotes_reducer", state, action);
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      return [...state.slice(0, index), ...state.slice(index + 1)];
+    case "UPVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state.slice(index, index + 1);
+      quote[0].votes += 1;
+      return [...state.slice(0, index), quote[0], ...state.slice(index + 1)];
+    case "DOWNVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state.slice(index, index + 1);
+      quote[0].votes > 0 ? (quote[0].votes -= 1) : null;
+      return [...state.slice(0, index), quote[0], ...state.slice(index + 1)];
+    default:
+      return state;
+  }
+};
